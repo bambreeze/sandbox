de9a2c609551cfd59e13bcb45bf223fc1903ba38
bambreeze <bambreeze@gmail.com>
2012-03-18 11:39:30 +0800

    update txt2reg and html2reg program

--- perl/txt2reg.pl
+++ perl/txt2reg.pl
@@ -6,13 +6,30 @@ open TXT_FILE, "registers.txt" or die $!;
 while (<TXT_FILE>) {
     chomp;
     my ($name, $address) = split /,/;
+    my $name_org = $name;
 
     my @name_a = split /\s+/, $name;
+    unless ($name_a[0]) {
+        shift @name_a;
+    }
+    foreach (@name_a) {
+        $_ = uc($_);
+    }
     $name = join "_", @name_a;
 
-    $table{$address} = $name;
+    if ($address =~ /0[xX]([\dA-Fa-f]+)/) {
+        #print "got it 1...", $1, "\n";
+        $address = "0x" . uc($1);
+        $table{$address} = $name;
+    } elsif ($address =~ /([\dA-Fa-f]+)/) {
+        #print "got it 2...", $1, "\n";
+        $address = "0x" . uc($1);
+        $table{$address} = $name;
+    } else {
+        print "Something worng! We need an address for this register '$name_org'\n";
+    }
 }
 
 foreach (sort keys %table) {
-    print "$_ => $table{$_}\n";
+    print "$table{$_} => $_\n";
 }
