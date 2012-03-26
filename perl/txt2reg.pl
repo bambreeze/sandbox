e51afaeef3f65256c5f72633b6cc44e18cf2080f
bambreeze <bambreeze@gmail.com>
2012-03-26 17:39:07 +0800

    simplify txt2reg program

--- perl/txt2reg.pl
+++ perl/txt2reg.pl
@@ -12,31 +12,16 @@ while (<TXT_FILE>) {
     next unless $_;
 
     my ($name, $address) = split /,/;
-    my $name_org = $name;
+    if ($address =~ /0[xX]([\dA-Fa-f]+)|([\dA-Fa-f]+)/) {
+        $address = sprintf "0x%08X", hex($&);
 
-    my @name_a = split /\s+/, $name;
-    unless ($name_a[0]) {
-        shift @name_a;
-    }
-    foreach (@name_a) {
-        $_ = uc($_);
-    }
-    $name = join "_", @name_a;
+        $name =~ s/\s+/_/g;
+        $name = uc($name);
 
-    if ($address =~ /0[xX]([\dA-Fa-f]+)/) {
-        #print "got it 1...", $1, "\n";
-        #$address = "0x" . uc($1);
-        #printf "0x%08X\n", hex($address);
-        $address = sprintf "0x%08X", hex($1);
-        $table{$address} = $name;
-    } elsif ($address =~ /([\dA-Fa-f]+)/) {
-        #print "got it 2...", $1, "\n";
-        #$address = "0x" . uc($1);
-        $address = sprintf "0x%08X", hex($1);
         $table{$address} = $name;
     } else {
         print "Something worng! We need an address for this register \
-        '$name_org'\n";
+        '$name'\n";
     }
 }
 
