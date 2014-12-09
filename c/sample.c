c4bd5254802a066001e684fa866312777b7cf5e9
bambreeze <bambreeze@gmail.com>
2014-12-09 15:53:16 +0800

    reorgnize the directory/file

--- c/sample.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    int a = 1;
-    printf("%d", a << 2 + a);
-    printf("%d", a << 5 + a);
-    return 0;
-}
--- test.c
+++ test.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include "test.h"
+
+unsigned int global_var[5] = {8};
 
 int add (int a, int b)
 {
@@ -15,11 +18,19 @@ int add (int a, int b)
 
 int main (void)
 {
+    int i = 0;
     int x = 2;
     int y = 3;
     int result = 0;
 
+    // add
     result = add (x, y);
-
     printf("The result is: %d\n", result);
+
+    // verify init of global variable
+    for (i = 0; i < 5; i++) {
+        printf("global variable init, data[%d]: %d\n", i, global_var[i]);
+    }
+
+    return 0;
 }
