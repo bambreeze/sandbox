8cc157b9115d1e7351d8d4827b0b18ce6b78fba2
bambreeze <bambreeze@gmail.com>
2016-01-01 21:11:20 +0800

    udpate errno utility

--- util/errno.c
+++ util/errno.c
@@ -1,14 +1,14 @@
-#include <stdio.h>  
-#include <string.h>  
-#include <errno.h>  
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
 
-int main()  
-{  
-    int i;  
-    for(i = 0; i < 140; ++i)  
-    {  
-        errno = i;  
-        printf("errno %d :\t\t%s\n",i,strerror(errno));  
-    }  
-    return 0;  
-}  
+int main()
+{
+    int i;
+    for(i = 0; i < 140; ++i) {
+        errno = i;
+        printf("errno %d:\t%s\n", i, strerror(errno));
+    }
+
+    return 0;
+}
