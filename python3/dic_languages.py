eaf02001f34d38611a761dbc28997751a0669d80
bambreeze <bambreeze@gmail.com>
2019-10-09 20:59:46 +0800

    Add exercises for dic operations

---
+++ python3/dic_languages.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+favorite_languages = {
+    'jen':'python',
+    'sarah':'c',
+    'edward':'ruby',
+    'phil':'python',
+    }
+
+for name, language in favorite_languages.items():
+    print(name.title() + "'s favorite language is " + language.title() + ".")
