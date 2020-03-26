0fbbeebff739dda6fb9dc5ac0cf1189b0edaabab
bambreeze <bambreeze@gmail.com>
2020-03-26 14:09:39 +0800

    add pexpect test

---
+++ pexpect/telnet_send_ctrl_char.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import sys
+import time
+import pexpect
+
+console_ip = "192.168.1.111"
+console_port = "2011"
+
+process = pexpect.spawn("telnet " + console_ip + " " + console_port)
+process.expect("Escape character is")
+process.sendline("\r")
+process.expect(">")
+process.sendline("?")
+process.expect(">")
+
+# quit the telnet gracefully
+process.sendcontrol(']')
+process.expect(">")
+process.sendline("q")
+process.expect("Connection closed")
+
+time.sleep(100)
+
