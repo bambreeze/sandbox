56ae5557db10809ff72f9035c8cd4d623c8f4c95
bambreeze <bambreeze@gmail.com>
2020-09-13 23:04:49 +0800

    update sk-house

--- ml/hands-on/sk-house.py
+++ ml/hands-on/sk-house.py
@@ -1,6 +1,10 @@
 # To support both python 2 and python 3
 from __future__ import division, print_function, unicode_literals
 
+# Ignore useless warnings (see SciPy issue #5998)
+import warnings
+warnings.filterwarnings(action="ignore", message="^internal gelsd")
+
 # Common imports
 import os
 import sys
@@ -9,10 +13,9 @@ import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
-# Ignore useless warnings (see SciPy issue #5998)
-import warnings
-warnings.filterwarnings(action="ignore", message="^internal gelsd")
-
+###############################################################################
+# Setup
+###############################################################################
 # Where to download the dataset
 DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
 HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
@@ -22,7 +25,6 @@ PROJECT_ROOT_DIR = sys.path[0]
 HOUSING_PATH = os.path.join(PROJECT_ROOT_DIR, "datasets", "housing")
 IMAGES_PATH = os.path.join(PROJECT_ROOT_DIR, "images", "housing")
 
-# Setup
 # to make this notebook's output stable across runs
 np.random.seed(42)
 
@@ -40,7 +42,9 @@ def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
         plt.tight_layout()
     plt.savefig(path, format=fig_extension, dpi=resolution)
 
-# Get the Data
+###############################################################################
+# Get the data
+###############################################################################
 import tarfile
 from six.moves import urllib
 
@@ -67,6 +71,101 @@ print(housing.describe())
 #%matplotlib inline
 housing.hist(bins=50, figsize=(20,15))
 save_fig("attribute_histogram_plots")
-plt.show()
+#plt.show()
 
-print("end")
+# For illustration only. Sklearn has train_test_split()
+def split_train_test(data, test_ratio):
+    shuffled_indices = np.random.permutation(len(data))
+    test_set_size = int(len(data) * test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+    return data.iloc[train_indices], data.iloc[test_indices]
+
+train_set, test_set = split_train_test(housing, 0.2)
+print(len(train_set), "train +", len(test_set), "test")
+
+from zlib import crc32
+import hashlib
+
+#def test_set_check(identifier, test_ratio):
+#    return crc32(np.int64(identifier)) & 0xffffffff < test_ratio * 2**32
+
+def test_set_check(identifier, test_ratio, hash=hashlib.md5):
+    return hash(np.int64(identifier)).digest()[-1] < 256 * test_ratio
+
+#def test_set_check(identifier, test_ratio, hash=hashlib.md5):
+#    return bytearray(hash(np.int64(identifier)).digest())[-1] < 256 * test_ratio
+
+def split_train_test_by_id(data, test_ratio, id_column):
+    ids = data[id_column]
+    in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio))
+    return data.loc[~in_test_set], data.loc[in_test_set]
+
+housing_with_id = housing.reset_index()   # adds an `index` column
+train_set, test_set = split_train_test_by_id(housing_with_id, 0.2, "index")
+
+housing_with_id["id"] = housing["longitude"] * 1000 + housing["latitude"]
+train_set, test_set = split_train_test_by_id(housing_with_id, 0.2, "id")
+print(test_set.head())
+
+from sklearn.model_selection import train_test_split
+train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
+print(test_set.head())
+
+housing["median_income"].hist()
+save_fig("median_income_histogram_plots")
+#plt.show()
+
+housing["income_cat"] = pd.cut(housing["median_income"],
+                               bins=[0., 1.5, 3.0, 4.5, 6., np.inf],
+                               labels=[1, 2, 3, 4, 5])
+print(housing["income_cat"].value_counts())
+
+housing["income_cat"].hist()
+save_fig("income_cat_histogram_plots")
+#plt.show()
+
+from sklearn.model_selection import StratifiedShuffleSplit
+
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+for train_index, test_index in split.split(housing, housing["income_cat"]):
+    strat_train_set = housing.loc[train_index]
+    strat_test_set = housing.loc[test_index]
+
+print(strat_test_set["income_cat"].value_counts() / len(strat_test_set))
+print(housing["income_cat"].value_counts() / len(housing))
+
+def income_cat_proportions(data):
+    return data["income_cat"].value_counts() / len(data)
+
+train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
+
+compare_props = pd.DataFrame({
+    "Overall": income_cat_proportions(housing),
+    "Stratified": income_cat_proportions(strat_test_set),
+    "Random": income_cat_proportions(test_set),
+}).sort_index()
+compare_props["Rand. %error"] = 100 * compare_props["Random"] / compare_props["Overall"] - 100
+compare_props["Strat. %error"] = 100 * compare_props["Stratified"] / compare_props["Overall"] - 100
+print(compare_props)
+
+for set_ in (strat_train_set, strat_test_set):
+    set_.drop("income_cat", axis=1, inplace=True)
+
+###############################################################################
+# Discover and visualize the data to gain insights
+###############################################################################
+housing = strat_train_set.copy()
+
+housing.plot(kind="scatter", x="longitude", y="latitude")
+save_fig("bad_visualization_plot")
+
+housing.plot(kind="scatter", x="longitude", y="latitude", alpha=0.1)
+save_fig("better_visualization_plot")
+
+housing.plot(kind="scatter", x="longitude", y="latitude", alpha=0.4,
+    s=housing["population"]/100, label="population", figsize=(10,7),
+    c="median_house_value", cmap=plt.get_cmap("jet"), colorbar=True,
+    sharex=False)
+plt.legend()
+save_fig("housing_prices_scatterplot")
